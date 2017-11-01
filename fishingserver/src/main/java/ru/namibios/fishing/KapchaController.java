13a230fc3377c5ef659da22f202a96f1c66b6135
symbios <dxeqtr@gmail.com>
2017-11-01 16:52:31 +0300

    Add monitoring

--- fishingserver/src/main/java/ru/namibios/fishing/KapchaController.java
+++ fishingserver/src/main/java/ru/namibios/fishing/KapchaController.java
@@ -1,20 +1,30 @@
 package ru.namibios.fishing;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import ru.namibios.fishing.model.Chars;
 import ru.namibios.fishing.model.ImageParser;
+import ru.namibios.fishing.utils.ImageUtils;
 import ru.namibios.fishing.utils.JSON;
 import ru.namibios.fishing.utils.Message;
 import ru.namibios.fishing.utils.ResponseHandler;
@@ -23,6 +33,8 @@ import ru.namibios.fishing.utils.Status;
 @Controller
 public class KapchaController {
 	
+	private static final String UPLOAD_DIR = "fishingserver/resources/";
+
 	private static final Logger logger = LoggerFactory.getLogger(KapchaController.class);
 
 	@Autowired
@@ -92,6 +104,64 @@ public class KapchaController {
 		
 		logger.info(String.format(Message.MSG_RESP_KEYS, hash, keys));
 		ResponseHandler.writeMapperJson(response, keys);
+		
+	}
+	
+	@RequestMapping(value="upload")
+	public void uploadImage(@RequestParam("HASH") String hash,
+							@RequestParam("SCREEN") MultipartFile multipartFile) throws JsonParseException, JsonMappingException, IOException {
+		
+		logger.info("Start upload file for [" + hash + "]");
+		
+	    int status = service.checkHash(hash);
+		if(status != Status.AUTH_OK) {
+			logger.info("[" + hash + "] - Authentification bad");
+			return;
+		}
+		
+		InputStream in = new ByteArrayInputStream(multipartFile.getBytes());
+		BufferedImage image = ImageIO.read(in);
+		
+		File file = new File(UPLOAD_DIR + hash + ".jpg");
+		ImageIO.write(image, "jpg", file);
+	}
+	
+	@RequestMapping(value="/status")
+	public void status(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		String hash = request.getParameter("HASH");
+	
+	    int status = service.checkHash(hash);
+		if(status != Status.AUTH_OK) {
+			logger.info("[" + hash + "] - Authentification bad");
+			return;
+		}
+				
+		String path = "";
+		String name = "";
+		File files = new File(UPLOAD_DIR);
+		for(File file : files.listFiles()){
+			if(file.isFile()) {
+				name = file.getName();
+				if(name.startsWith(hash)) 
+					path = file.getAbsolutePath();
+			}
+		}
+		
+		if(path.isEmpty()) return;
+		
+		BufferedImage image = ImageUtils.read(new File(path));
+		byte[] bytes = ImageUtils.imageToBytes(image);
+		
+		response.setContentLength(bytes.length);
+		response.setContentType("image/jpeg");
+		response.setHeader("Content-Disposition", "attachment; filename=" + name);
+		
+		OutputStream outputStream = response.getOutputStream();
+		
+		outputStream.write(bytes, 0, bytes.length);
+		outputStream.flush();
+		outputStream.close();
 		
 	}
 	
